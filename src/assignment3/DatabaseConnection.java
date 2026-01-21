fed2f8b720dc63173ae51c7f576dcd6bb0734e5c
erasylbaiqonyr <erasylbaiqonyr@gmail.com>
2026-01-21 15:04:24 +0500

    era

---
+++ src/assignment3/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package assignment3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
+
+    private static final String USER =
+            "postgres.axcgckktfembduyzkvrs";
+
+    private static final String PASSWORD =
+            "AituDb_2026!";
+
+    private DatabaseConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
