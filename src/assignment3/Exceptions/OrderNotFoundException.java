45c4148a42a9a234f36b1b23ed448b571b146d7f
Kengesov Nurgazi <nurgazi2023@gmail.com>
2026-01-22 14:50:47 +0500

    Kengesov Nurgazi: Minor changes

--- src/assignment3/Exceptions/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package assignment3.Exceptions;
-
-public class OrderNotFoundException extends RuntimeException{
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
